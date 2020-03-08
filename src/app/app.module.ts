141838346979196391dbff76eff995a9d31263cf
zimo <1040099868@qq.com>
2020-03-08 15:54:49 +0800

    文件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminProductComponent } from './components/admin-product/admin-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminProductComponent
   ],
   imports: [
     BrowserModule,
